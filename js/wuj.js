0b39893e90bfc3f9518789d98a9c61252b8a8d44
Matt Wujek <mattwujek@gmail.com>
2015-04-14 07:51:39 -0700

    initial commit
    …

---
+++ js/wuj.js
@@ -0,0 +1,11 @@
+// $('.btn-list').each(function(){
+// 	listEl = $(this).find('li');
+// 	hiddenEl = $(this).find('.hidden-item');
+// 	console.log(hiddenEl);
+// 	ogWidth = hiddenEl.innerWidth();
+// 	hiddenEl.velocity({width:100},{duration:0});
+// 	listEl.click(function(event) {
+// 		console.log('asd');
+// 		hiddenEl.velocity({width: ogWidth},{duration: 500});
+// 	});
+// });
